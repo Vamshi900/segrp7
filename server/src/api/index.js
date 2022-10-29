b7f962a482afe7b2395631d66d7c1d90f79a0ea1
Wi11iamC <willi4m553@gmail.com>
2022-10-29 01:28:49 -0400

    :snake: added orders route

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const orders = require("./orders/orders.routes")
+
 
 const router = express.Router();
 
+// API overview:
 router.get("/", (req, res) => {
   res.json({
-    message: "Hello World",
+    message: "Hello World (API Overview)",
   });
 });
+
+
+router.use('/orders', orders)
+
+
 
 module.exports = router;
