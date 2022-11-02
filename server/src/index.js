e16deaf6ee01740e01fc8603571b79884fd64caf
Advait Bhat <advaitbhat20@gmail.com>
2022-11-02 12:40:19 -0400

    added DB connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,21 @@
 const app = require('./app');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host:process.env.DB_HOST,
+  port:process.env.DB_PORT,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
+})
+
+db.connect(err=>{
+  if(err) {
+    console.log(err.message);
+    return;
+  }
+  console.log("Connected to Database");
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
