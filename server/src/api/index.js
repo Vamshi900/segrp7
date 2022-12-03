3f5ad2f6e5a60ed3c8a1be26a4db1ab17c5234dc
Vamshi900 <vamshigeek1@gmail.com>
2022-12-03 00:52:01 -0500

    user login

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -5,6 +5,7 @@ const users = require("./users/users.routes");
 const saved_for_later = require("./save_for_later/saved.routes");
 const cart = require("./cart/cart.routes");
 const reviews = require("./reviews/reviews.routes");
+const auth = require("./auth/auth.routes");
 
 
 
@@ -24,6 +25,7 @@ router.use('/users', users);
 router.use('/saved_for_later', saved_for_later);
 router.use('/cart', cart);
 router.use('/reviews', reviews);
+router.use('/auth', auth);
 
 
 
